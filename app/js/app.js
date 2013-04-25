00aba6543adf0012c8297c9626044f1468db3f46
Rodney Riggs <Rodney@SoftLore.com>
2013-04-24 22:12:19 -0600

    Login page functioning
    Got the login dialog working as a modal
    Added Angular-UI (instead of Bootstrap.js)

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
+    // $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/view1'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,14 +3,48 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+	controller('MyCtrl1', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
+	}])
+	.controller('MyCtrl2', [function() {
 
-  }])
-  .controller('LoginCtrl', ['$scope', function ($scope) {
-  	$scope.username = "fred";
-  	$scope.password = "empty";
-  }]);
+	}])
+	.controller('LoginCtrl', ['$scope', '$dialog',  function ($scope, $dialog) {
 
+var t = '<div class="modal-header">'+
+          '<h1>This is the title</h1>'+
+          '</div>'+
+          '<div class="modal-body">'+
+          '<p>Enter a value to pass to <code>close</code> as the result: <input ng-model="result" /></p>'+
+          '</div>'+
+          '<div class="modal-footer">'+
+          '<button ng-click="close(result)" class="btn btn-primary" >Close</button>'+
+          '</div>';
+
+	$scope.opts = {
+		backdrop: true,
+		keyboard: true,
+		backdropClick: true,
+		templateUrl: 'partials/login.html',
+		// template: t,
+		controller: 'LoginController'
+	};
+
+	$scope.doLogin = function(){
+		var d = $dialog.dialog($scope.opts);
+		d.open().then(function(result){
+			if(result)
+			{
+				alert('dialog closed with result: ' + result);
+			}
+		});
+	};
+}]);
+
+// the dialog is injected in the specified controller
+function LoginController($scope, dialog){
+	$scope.close = function(result){
+		dialog.close(result);
+	};
+
+}
