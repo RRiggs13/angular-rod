1cde0b39c98a7370ff7c432fb73d5dee7d4267e1
Rodney Riggs <Rodney@SoftLore.com>
2013-05-07 08:46:14 -0600

    Shared login data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,34 +6,43 @@ angular.module('myApp.controllers', []).
 	controller('MyCtrl1', [function() {
 
 	}])
-	.controller('Questionaire', [function() {
+	.controller('Questionaire', ['$scope', '$rootScope', function($scope, $rootScope) {
 
+		$scope.labelClick = function(){
+debugger;
+			alert('username: ' + $scope.auth);
+		};
 	}])
-	.controller('LoginCtrl', ['$scope', '$dialog',  function ($scope, $dialog) {
+	.controller('RootCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+debugger;
+		$scope.auth = 'Fred';
+	}])
+	.controller('LoginCtrl', ['$scope', '$dialog', '$rootScope',  function ($scope, $dialog, $rootScope) {
 
-	$scope.opts = {
-		backdrop: true,
-		keyboard: true,
-		backdropClick: true,
-		backdropFade: true,
-		dialogFade: true,
-		templateUrl: 'partials/login.html',
-		controller: 'LoginController'
-	};
+		$scope.opts = {
+			backdrop: true,
+			keyboard: true,
+			backdropClick: true,
+			backdropFade: true,
+			dialogFade: true,
+			templateUrl: 'partials/login.html',
+			controller: 'LoginController'
+		};
 
-	$scope.doLogin = function(){
-		var d = $dialog.dialog($scope.opts);
-		d.open().then(function(result){
-			if(result)
-			{
-				alert('dialog closed with result: ' + result);
-			}
-		});
-	};
-}]);
+		$scope.doLogin = function(){
+			var d = $dialog.dialog($scope.opts);
+			d.open().then(function(result){
+				if(result)
+				{
+					$rootScope.auth = result;
+					alert('dialog closed with result: ' + result);
+				}
+			});
+		};
+	}]);
 
 // the dialog is injected in the specified controller
-function LoginController($scope, dialog){
+function LoginController($scope, $rootScope, dialog){
 	$scope.close = function(result){
 		dialog.close(result);
 	};
